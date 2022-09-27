ee39115dbd56e83d75590511cad60a7deb5f81d3
jirka <jiri.work@outlook.com>
2022-09-27 15:33:59 +0200

    Added support for Binance spot screening

--- crypto_screener/data_downloader.py
+++ crypto_screener/data_downloader.py
@@ -11,14 +11,22 @@ class DataDownloader:
     def __init__(self, rate_exceed_delay_seconds):
         self.phemex_client = ccxt.phemex()
         self.kucoin_client = ccxt.kucoin()
+        self.binance_client = ccxt.binance()
         self.rate_exceed_delay_seconds = rate_exceed_delay_seconds
 
     def download_daily_ohlc(self, exchange, ticker):
         logging.debug("Start downloading daily OHLC for {} on exchange {}".format(ticker, exchange))
-        if exchange == "Phemex":
-            return self.__download_daily_ohlc_from_ccxt("Phemex", ticker.replace("PERP", "").replace("100", "u100"))
-        else:
-            return self.__download_daily_ohlc_from_ccxt("Kucoin", ticker.replace("USDT", "-USDT"))
+        if exchange == "PhemexFutures":
+            return self.__download_daily_ohlc_from_ccxt(self.phemex_client,
+                                                        ticker.replace("PERP", "").replace("100", "u100"))
+
+        if exchange == "KucoinSpot":
+            return self.__download_daily_ohlc_from_ccxt(self.kucoin_client, ticker.replace("USDT", "-USDT"))
+
+        if exchange == "BinanceSpot":
+            return self.__download_daily_ohlc_from_ccxt(self.binance_client, ticker)
+
+        raise Exception("Not supported exchange - {}".format(exchange))
 
     def __download_daily_ohlc_from_crypto_data_download(self, exchange, ticker):
         ohlc_daily = pd.read_csv(self.CRYPTO_DATA_DOWNLOADER_URL.format(exchange, ticker),
@@ -28,10 +36,9 @@ class DataDownloader:
         ohlc_daily.sort_values(["date"], ascending=True, inplace=True)
         return ohlc_daily[["open", "high", "low", "close"]]
 
-    def __download_daily_ohlc_from_ccxt(self, exchange, ticker):
+    def __download_daily_ohlc_from_ccxt(self, exchange_client, ticker):
         while True:
             try:
-                exchange_client = self.phemex_client if exchange == "Phemex" else self.kucoin_client
                 ohlc_daily_raw = exchange_client.fetch_ohlcv(ticker, timeframe="1d", limit=200)
                 ohlc_daily = pd.DataFrame(ohlc_daily_raw, columns=["date", "open", "high", "low", "close", "volume"])
                 ohlc_daily["date"] = pd.to_datetime(ohlc_daily["date"], unit='ms')
