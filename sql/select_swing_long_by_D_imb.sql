14652a227a1b1a0bcf26a70b35d0c5ad2eb3fbb3
jirka <jiri.work@outlook.com>
2022-10-11 20:35:15 +0200

    added select_swing_long_by_D_imb.sql and select_swing_short_by_D_imb.sql

---
+++ sql/select_swing_long_by_D_imb.sql
@@ -0,0 +1,27 @@
+SELECT
+  bs.ticker,
+  bs.exchange,
+  bi.imb_buy_D_date,
+  bi.imb_buy_D_price,
+  bi."imb_buy_D_distance%",
+  bs.last_price,
+  bs.last_date,
+  bs.oscillators_rating,
+  td.analysed_swing
+FROM
+  base_screening bs,
+  buyer_imbalances bi,
+  trader_data td
+WHERE
+  bs.ticker = bi.ticker
+  AND bs.exchange = bi.exchange
+  AND bs.ticker = td.ticker
+  AND bs.exchange = td.exchange
+  AND bs.exchange = 'PhemexFutures'
+  AND bs.volatility_rating in ('MEDIUM', 'HIGH')
+  AND bs.oscillators_rating in ('BEARISH', 'OVERBOUGHT')
+  AND td.analysed_swing = 0
+  AND bi."imb_buy_D_distance%" < 0.2
+  AND bi.imb_buy_D_date < DATE('now', '-5 day')
+ORDER BY
+  bi."imb_buy_D_distance%" ASC
