7f621dc704040f9841b176098ffb79bd10ccfbe4
lucka <lucie.cagova@proton.me>
2022-10-17 16:25:25 +0200

    feature: added sql query to check M long imbalances on Okx exchange

---
+++ sql/select_position_long_by_M_imb.sql
@@ -0,0 +1,25 @@
+ SELECT
+  bs.ticker,
+  bi.imb_buy_M_date,
+  bi.imb_buy_M_price,
+  bi."imb_buy_M_distance%",
+  bs.last_price,
+  bs.sma_20,
+  bs.sma_50,
+  bs.sma_200,
+  bs.moving_averages_rating
+FROM
+  base_screening bs,
+  buyer_imbalances bi
+WHERE
+  bs.ticker = bi.ticker
+  and bs.exchange = bi.exchange
+  AND bs.exchange = 'OkxSpot'
+  AND bs.last_price > 0.01
+  AND bs.moving_averages_rating IN (
+    'STRONG_DOWN_TREND', 'DOWN_TREND'
+  )
+  and bi."imb_buy_M_distance%" > 0
+  and bi."imb_buy_M_distance%" < 0.5
+order BY
+  bi."imb_buy_M_distance%"
