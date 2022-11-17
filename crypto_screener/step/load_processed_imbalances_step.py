6d821e569a5efd2cc83f4e48e62e92ccd5270f52
jirka <jiri.caga@proton.me>
2022-11-17 17:08:21 +0100

    Removed save buyer and seller interest imb to db

--- crypto_screener/step/load_processed_imbalances_step.py
+++ crypto_screener/step/load_processed_imbalances_step.py
@@ -11,8 +11,6 @@ class LoadProcessedImbalancesStep:
         step_config = config["steps"]["loadProcessedImbalancesStep"]
         self.buyer_imbalances_processed_path = step_config["buyerImbalancesProcessedPath"]
         self.seller_imbalances_processed_path = step_config["sellerImbalancesProcessedPath"]
-        self.buyer_imbalances_interest_path = step_config["buyerImbalancesInterestPath"]
-        self.seller_imbalances_interest_path = step_config["sellerImbalancesInterestPath"]
         self.crypto_screener_db_conn = crypto_screener_db_conn
 
     def process(self):
@@ -22,18 +20,11 @@ class LoadProcessedImbalancesStep:
 
         buyer_imbalances_processed = pd.read_csv(self.buyer_imbalances_processed_path)
         seller_imbalances_processed = pd.read_csv(self.seller_imbalances_processed_path)
-        buyer_imbalances_interest = pd.read_csv(self.buyer_imbalances_interest_path)
-        seller_imbalances_interest = pd.read_csv(self.seller_imbalances_interest_path)
 
         buyer_imbalances_processed.to_sql(name="buyer_imbalances_processed", con=self.crypto_screener_db_conn,
                                           if_exists="replace")
         seller_imbalances_processed.to_sql(name="seller_imbalances_processed", con=self.crypto_screener_db_conn,
                                            if_exists="replace")
-
-        buyer_imbalances_interest.to_sql(name="buyer_imbalances_interest", con=self.crypto_screener_db_conn,
-                                         if_exists="replace")
-        seller_imbalances_interest.to_sql(name="seller_imbalances_interest", con=self.crypto_screener_db_conn,
-                                          if_exists="replace")
 
         logging.info(SEPARATOR)
         logging.info("Finished load processed imbalances to database step")
